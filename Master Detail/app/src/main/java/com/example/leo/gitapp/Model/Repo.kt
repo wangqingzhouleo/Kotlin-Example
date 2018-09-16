f6e8b41d1c0c0ba8fdeda90c1e717ba6db158e85
leo.wang <wangqingzhouleo@gmail.com>
2018-09-16 22:57:55 +1000

    add Repo model

---
+++ Master Detail/app/src/main/java/com/example/leo/gitapp/Model/Repo.kt
@@ -0,0 +1,44 @@
+package com.example.leo.gitapp.Model
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.json.JSONObject
+
+class Repo() : Parcelable {
+
+    private var _name: String = ""
+    var name: String
+        get() { return name.toUpperCase() }
+        set(value) { _name = value }
+
+    var htmlUrl: String = ""
+
+    constructor(data: JSONObject) : this() {
+        name = data.getString("name")
+        htmlUrl = data.getString("html_url")
+    }
+
+    constructor(parcel: Parcel) : this() {
+        name = parcel.readString()
+        htmlUrl = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(htmlUrl)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Repo> {
+        override fun createFromParcel(parcel: Parcel): Repo {
+            return Repo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Repo?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
