65de70672edfbf4a2b50e1cea711b4a89880d9ca
leo.wang <wangqingzhouleo@gmail.com>
2018-09-16 22:57:13 +1000

    add User model

---
+++ Master Detail/app/src/main/java/com/example/leo/gitapp/Model/User.kt
@@ -0,0 +1,52 @@
+package com.example.leo.gitapp.Model
+
+import android.os.Parcel
+import android.os.Parcelable
+import org.json.JSONObject
+
+class User(): Parcelable {
+
+    var id = 0
+    var name = ""
+    var avatarUrl = ""
+    var publicRepoCount = 0
+    var reposUrl = ""
+
+    constructor(data: JSONObject) : this() {
+        id = data.getInt("id")
+        name = data.getString("name")
+        avatarUrl = data.getString("avatar_url")
+        publicRepoCount = data.getInt("public_repos")
+        reposUrl = data.getString("repos_url")
+    }
+
+    constructor(parcel: Parcel) : this() {
+        id = parcel.readInt()
+        name = parcel.readString()
+        avatarUrl = parcel.readString()
+        publicRepoCount = parcel.readInt()
+        reposUrl = parcel.readString()
+    }
+
+    override fun writeToParcel(p0: Parcel?, p1: Int) {
+        p0?.writeInt(id)
+        p0?.writeString(name)
+        p0?.writeString(avatarUrl)
+        p0?.writeInt(publicRepoCount)
+        p0?.writeString(reposUrl)
+    }
+
+    override fun describeContents(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
